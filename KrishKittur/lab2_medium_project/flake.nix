57187e6c76125fbb4d026acc1b83ceb8627f0476
Krish Kittur <krishkittur@lawn-128-61-113-14.lawn.gatech.edu>
2024-09-13 14:37:18 -0400

    Fixed nix files

--- KrishKittur/lab2_medium_project/flake.nix
+++ KrishKittur/lab2_medium_project/flake.nix
@@ -1,23 +1,21 @@
-description = "lab 1 flake";
 
-inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-};
-
-outputs = { self, nixpkgs, ... }:
-let 
-    hello_lib_overlay = final: prev: {
-        hello_lib = final.callPackage ./default.nix { };
+{ 
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     };
 
-    my_overlays = [ hello_lib_overlay ]
-
-    pkgs = import nixpkgs {
-        system = "x86_64-linux";
-
-        overlays = [ self.overlays.default ];
+    outputs = { self, nixpkgs, ... }:
+    let 
+        hello_lib_overlay = final: prev: {
+            hello_lib = final.callPackage ./default.nix { };
+        };
+        my_overlays = [ hello_lib_overlay ];
+        pkgs = import nixpkgs {
+            system = "x86_64-linux";
+            overlays = [ self.overlays.default ];
+        };
+    in {
+        packages.x86_64-linux.default = pkgs.hello_lib;
+        overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
     };
-in {
-    packages.x86_64-linux.default = pkgs.hello_lib;
-    overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
 }
