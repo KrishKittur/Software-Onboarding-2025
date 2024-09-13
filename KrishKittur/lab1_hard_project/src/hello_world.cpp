7741a4204a1c9363cfe6e60cc8865188a537af94
Krish Kittur <krishkittur@lawn-128-61-113-14.lawn.gatech.edu>
2024-09-13 12:06:52 -0400

    Completed lab1

---
+++ KrishKittur/lab1_hard_project/src/hello_world.cpp
@@ -0,0 +1,8 @@
+#include <hello_world.hpp>
+
+#include <iostream>
+
+void say_hello() 
+{
+    std::cout << "Hello, World!\n";
+}
---
+++ KrishKittur/lab1_hard_project/src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <hello_world.hpp>
+
+int main() 
+{
+    say_hello();
+}
